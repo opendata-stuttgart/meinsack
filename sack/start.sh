d199dd77b1a2ad77d1a45ab47695bfa70afd8d38
Andreas Madsack <andreas@madflex.de>
2015-11-14 16:58:37 +0100

    initial project

---
+++ sack/start.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+export DJANGO_SETTINGS_MODULE=sack.settings.production
+export PYTHONUNBUFFERED=0
+mkdir -p /home/uid1000/sack
+mkdir -p /home/uid1000/sack/logs
+mkdir -p /home/uid1000/sack/run
+chmod -R 777 /home/uid1000/sack/run
+python3 manage.py migrate
+python3 manage.py collectstatic --noinput
+gunicorn sack.wsgi:application --log-level=info --bind=unix:/home/uid1000/sack/run/server.sock -w 3
