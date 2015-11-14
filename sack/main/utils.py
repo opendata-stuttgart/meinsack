be3be9b9cc4fa9954982e80e69e6304e12fe49ac
Andreas Madsack <andreas@madflex.de>
2015-11-14 18:02:18 +0100

    district import stuff

--- sack/main/utils.py
+++ sack/main/utils.py
@@ -1,9 +1,29 @@
 from bs4 import BeautifulSoup
 import requests
 
-def get_districts_stuttgart(url=None):
-    if not url:
+
+def get_districts_stuttgart(data_source=None):
+    if not data_source:
         url = 'http://onlinestreet.de/strassen/in-Stuttgart.html'
-    data = requests.get(url)
-    soup = BeautifulSoup(data.text, 'html.parser')
-    print(soup)
+        data = requests.get(url).text
+    else:
+        data = open(data_source).read()
+    soup = BeautifulSoup(data, 'html.parser')
+    for table in soup.findAll('table'):
+        if 'blz' in table.get("class"):
+            for tr in table.findAll('tr'):
+                yield tr
+
+
+def extract_data_from_tr(tr):
+    data = list(tr.children)
+    return {
+        'name': data[0].a.text,
+        'url_onlinestreet': data[0].a['href'],
+        'city': 'Stuttgart',
+        'number_of_streets': data[1].text
+    }
+
+
+def add_district_to_database(data):
+    pass
--- sack/tests/test_district.py
+++ sack/tests/test_district.py
@@ -1,7 +1,37 @@
+import os.path
 import pytest
+from main.utils import get_districts_stuttgart, add_district_to_database, extract_data_from_tr
+
+
+@pytest.fixture
+def stuttgart_districts():
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'fixtures/',
+                        'stuttgart_districts.html')
 
 
 class TestDistrict():
 
-    def test_district_import(self):
-        pass
+    def test_district_import(self, stuttgart_districts):
+        x = get_districts_stuttgart(stuttgart_districts)
+        assert len(list(x)) == 57
+
+    def test_extract_data_from_tr(self, stuttgart_districts):
+        for index, tr in enumerate(get_districts_stuttgart(stuttgart_districts)):
+            if index == 0:
+                # ignore head
+                continue
+            x = extract_data_from_tr(tr)
+            assert isinstance(x, dict)
+            assert 'name' in x
+            break  # one is enough
+
+    def test_add_district_to_database(self, stuttgart_districts):
+        for index, tr in enumerate(get_districts_stuttgart(stuttgart_districts)):
+            if index == 0:
+                # ignore head
+                continue
+            x = extract_data_from_tr(tr)
+            add_district_to_database(x)
+            # FIXME
+            break  # one is enough
