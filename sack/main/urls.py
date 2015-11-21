919eca6f31fafadab5631e1796639d13e14559cf
Andreas Madsack <andreas@madflex.de>
2015-11-21 17:02:00 +0100

    first api views for zipcodes

--- sack/main/urls.py
+++ sack/main/urls.py
@@ -2,11 +2,15 @@
 from rest_framework import routers
 from django.conf.urls import patterns, include, url
 
+from .views import ZipCodeDetailView, ZipCodeListView
+
 router = routers.DefaultRouter()
 
 urlpatterns = patterns(
     '',
-    url(
-        regex=r'^', view=include(router.urls)
-    ),
+#    url(
+#        regex=r'^', view=include(router.urls),
+#    ),
+    url(r'^$', ZipCodeListView.as_view()),
+    url(r'^(?P<zipcode>\d{5})/$', ZipCodeDetailView.as_view()),
 )
--- sack/main/views.py
+++ sack/main/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+from rest_framework import generics
 
-# Create your views here.
+from rest_framework import serializers
+from .models import Street
+
+
+class ZipCodeListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Street
+        fields = [
+            'zipcode', 'city'
+        ]
+        read_only_fields = fields
+
+
+class ZipCodeDetailSerializer(serializers.ModelSerializer):
+    street = serializers.SerializerMethodField()
+
+    def get_street(self, obj):
+        return obj.name
+
+    class Meta:
+        model = Street
+        fields = [
+            'zipcode', 'city', 'street'
+        ]
+        read_only_fields = fields
+
+
+class ZipCodeListView(generics.ListAPIView):
+    authentication_classes = list()
+    permission_classes = list()
+    serializer_class = ZipCodeListSerializer
+
+    def get_queryset(self):
+        return Street.objects.distinct('zipcode').order_by('zipcode')
+
+
+class ZipCodeDetailView(generics.ListAPIView):
+    authentication_classes = list()
+    permission_classes = list()
+    serializer_class = ZipCodeDetailSerializer
+
+    def get_queryset(self):
+        return Street.objects.filter(zipcode=self.kwargs['zipcode'])
