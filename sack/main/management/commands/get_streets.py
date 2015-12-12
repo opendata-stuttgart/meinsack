2790d5b96ac61b35d0b2370c06a7b67daee3009e
Andreas Madsack <andreas@madflex.de>
2015-12-12 20:29:04 +0100

    pep8

--- sack/main/management/commands/get_streets.py
+++ sack/main/management/commands/get_streets.py
@@ -7,9 +7,15 @@ class Command(BaseCommand):
     help = "Import streets and districts"
 
     def handle(self, *args, **options):
-        from main.utils import get_districts_stuttgart, extract_district_from_tr, add_district_to_database, get_streets_from_district, extract_street_from_tr, add_street_to_database 
+        from main.utils import (
+            add_district_to_database,
+            add_street_to_database,
+            extract_district_from_tr,
+            extract_street_from_tr,
+            get_districts_stuttgart,
+            get_streets_from_district,
+        )
 
-        foo = False
         for tr in get_districts_stuttgart():
             d = extract_district_from_tr(tr)
             district = add_district_to_database(d)
