8791cbb07033360ecd98f8c779bfb600dd401b01
Andreas Madsack <andreas@madflex.de>
2015-12-29 15:21:41 +0100

    try to expose port

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 
 docker build --tag=meinsack-prod .
 docker rm -f meinsack
-docker run -d --volumes-from home --link meinsack-db:db -v `pwd`/meinsack/meinsack/settings/production.py:/opt/code/meinsack/meinsack/settings/production.py --restart=always --name meinsack meinsack-prod
+docker run -d --volumes-from home --link meinsack-db:db -v `pwd`/meinsack/meinsack/settings/production.py:/opt/code/meinsack/meinsack/settings/production.py --restart=always -p 8010:8010  --name meinsack meinsack-prod
