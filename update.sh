593fcddff5c404ef40f58496eee9106a826f505f
Andreas Madsack <andreas@madflex.de>
2015-12-29 15:31:21 +0100

    git pull in update script

--- update.sh
+++ update.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+git pull
+
 docker build --tag=meinsack-prod .
 docker rm -f meinsack
 docker run -d --volumes-from home --link meinsack-db:db -v `pwd`/meinsack/meinsack/settings/production.py:/opt/code/meinsack/meinsack/settings/production.py --restart=always -p 8010:8010  --name meinsack meinsack-prod
