d75777020251fb80cf76a191e544ead0774323e6
Andreas Madsack <andreas@madflex.de>
2015-12-29 19:25:35 +0100

    run notification at end of deployment

--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ git pull
 docker build --tag=meinsack-prod .
 docker rm -f meinsack
 docker run -d --volumes-from home --link meinsack-db:db -v `pwd`/meinsack/meinsack/settings/production.py:/opt/code/meinsack/meinsack/settings/production.py --restart=always -p 8010:8010  --name meinsack meinsack-prod
+
+sh notify.sh
