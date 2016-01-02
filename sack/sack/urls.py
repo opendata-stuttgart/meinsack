14e493f45453e743d93d6ad646c54ba231e7b708
Andreas Madsack <andreas@madflex.de>
2016-01-02 17:41:53 +0100

    redirect /v1 to /v1/

--- sack/sack/urls.py
+++ sack/sack/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from rest_framework.authtoken.views import obtain_auth_token
+from django.views.generic.base import RedirectView
 from main.views import GetIcalView
 
 
@@ -8,6 +9,7 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', GetIcalView.as_view(), name='get_ical'),
+    url(r'^v1$', RedirectView.as_view(url='/v1/', permanent=False)),
     url(r'^v1/', include('main.urls')),
     url(r'^auth/', include('rest_framework.urls',
                            namespace='rest_framework')),
