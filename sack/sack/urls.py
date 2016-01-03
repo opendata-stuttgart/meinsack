bfffe2c3a56d9a41b54d9bb265690aa85bb1be05
Andreas Madsack <andreas@madflex.de>
2016-01-03 23:04:22 +0100

    legal and faq added

--- sack/sack/urls.py
+++ sack/sack/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rest_framework.authtoken.views import obtain_auth_token
 from django.views.generic.base import RedirectView
+from django.views.generic import TemplateView
+from rest_framework.authtoken.views import obtain_auth_token
+
 from main.views import GetIcalView
 
 
@@ -9,6 +11,8 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
     url(r'^$', GetIcalView.as_view(), name='get_ical'),
+    url(r'^faq/$', TemplateView.as_view(template_name='faq.html'), name='faq'),
+    url(r'^legal/$', TemplateView.as_view(template_name='legal.html'), name='legal'),
     url(r'^v1$', RedirectView.as_view(url='/v1/', permanent=False)),
     url(r'^v1/', include('main.urls')),
     url(r'^auth/', include('rest_framework.urls',
