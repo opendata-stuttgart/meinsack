47f30480442e69aabe8be3ea8c802074992addfd
Andreas Madsack <andreas@madflex.de>
2017-12-30 14:02:37 +0100

    update middlewares and cors-headers

--- sack/sack/settings/base.py
+++ sack/sack/settings/base.py
@@ -34,17 +34,16 @@ INSTALLED_APPS = (
     'main',
 )
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+]
 
 ROOT_URLCONF = 'sack.urls'
 
